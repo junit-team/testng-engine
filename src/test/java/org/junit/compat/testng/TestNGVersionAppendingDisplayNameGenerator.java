95a4523adb3e5448477ade3effdf63aef8cea615
Marc Philipp <mail@marcphilipp.de>
2021-04-14 18:00:07 +0200

    Append TestNG version to all test names for IDE use

---
+++ src/test/java/org/junit/compat/testng/TestNGVersionAppendingDisplayNameGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.compat.testng;
+
+import java.lang.reflect.Method;
+import java.text.MessageFormat;
+
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+public class TestNGVersionAppendingDisplayNameGenerator extends DisplayNameGenerator.Standard {
+
+	@Override
+	public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+		var regularDisplayName = super.generateDisplayNameForMethod(testClass, testMethod);
+		return MessageFormat.format("{0} [{1}]", regularDisplayName, System.getProperty("testng.version"));
+	}
+}
