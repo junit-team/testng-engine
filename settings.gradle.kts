9f7335d5855f47c41027062d1573d80abbdae492
Marc Philipp <mail@marcphilipp.de>
2021-04-28 20:35:56 +0200

    Configure publication of build scans to ge.junit.org

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+
+plugins {
+    id("com.gradle.enterprise") version "3.6.1"
+}
+
 rootProject.name = "testng-engine"
+
+gradleEnterprise {
+    buildScan {
+        server = "https://ge.junit.org"
+        isCaptureTaskInputFiles = true
+        isUploadInBackground = System.getenv("CI") == null
+        this as BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
+    }
+}
