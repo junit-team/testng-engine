8b438e0d82a6d90f92d32ee313403c318590862e
Marc Philipp <mail@marcphilipp.de>
2021-07-09 15:25:55 +0200

    Add failing test

---
+++ src/testFixtures/java/example/dataproviders/ParallelDataProviderTestCase.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.dataproviders;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ParallelDataProviderTestCase {
+
+	@DataProvider(name = "numbers", parallel = true)
+	public static Iterator<Object[]> numbers() {
+		return IntStream.of(1, 3, 5, 7, 9, 11, 13, -5, -3, 15, Integer.MAX_VALUE) //
+				.mapToObj(Arrays::asList) //
+				.map(Collection::toArray) //
+				.iterator();
+	}
+
+	@Test(dataProvider = "numbers")
+	public void test(Integer number) {
+	}
+
+}
