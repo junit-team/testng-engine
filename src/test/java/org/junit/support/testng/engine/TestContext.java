57583157ab56978e8844b045bf446a7becb8c56a
Marc Philipp <mail@marcphilipp.de>
2023-01-06 10:44:07 +0100

    Add test coverage for 7.7.1 and 7.8.0-SNAPSHOT

--- src/test/java/org/junit/support/testng/engine/TestContext.java
+++ src/test/java/org/junit/support/testng/engine/TestContext.java
@@ -17,6 +17,6 @@ import org.apache.maven.artifact.versioning.ComparableVersion;
 class TestContext {
 
 	static ComparableVersion testNGVersion() {
-		return new ComparableVersion(removeEnd(System.getProperty("testng.version", "7.6.1"), "-SNAPSHOT"));
+		return new ComparableVersion(removeEnd(System.getProperty("testng.version", "7.7.1"), "-SNAPSHOT"));
 	}
 }
